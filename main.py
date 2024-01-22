6776a6561f7cdf252ce9c62f5a2ec6d101818514
lira_bot <lira@cs.bilkent.edu.tr>
2024-01-22 16:44:35 +0300

    planner pipeline improvement

--- main.py
+++ main.py
@@ -21,7 +21,9 @@ correct_graph_edge_indices(target_graph)
 target_pyg = [convert_to_pyg_data(graph) for graph, positions in zip(target_graph, pos)][0]
 test_data = target_pyg
 target_folder = 'target'
-building_order = create_plan(target_folder,model)
+
+plan = create_plan(target_folder,model,option=1)
+building_order = plan[0]
 
 print("Building Order:", building_order)
 object_number = len(building_order)
@@ -30,29 +32,39 @@ init_komo(object_number,C,pos)
 num_objects = len(building_order)
 objective_realized =False
 
+completed_checkpoints = []
 checkpoint_counter = 0
+
 while objective_realized != True:
     if checkpoint_counter == 0:
         komo = ry.KOMO(C, 3, 30, 1, False)
-        define_optimization(C,checkpoint_counter,komo)
+        obj_index = building_order[checkpoint_counter]
+        define_optimization(C,obj_index,komo)
         ret = ry.NLP_Solver(komo.nlp(), verbose=0).solve()
         q = komo.getPath()
     if ret.feasible:
         checkpoint_counter += 1
-        if checkpoint_counter == num_objects:
+        graph = plan[1]
+        plan = create_plan(target_folder,model,graph=graph,option=2)
+        building_order = plan[0]
+        if checkpoint_counter == num_objects -1:
             print("Planning Completed")
             print("Setting joint states...")
             for t in range(q.shape[0]):
                 C.setJointState(q[t])
                 time.sleep(.1)
-                print("Completed")
+            print("Completed")
             objective_realized =True
             break
-        komo = ry.KOMO(C, 3, 30, 1, False)
-        define_optimization(C,checkpoint_counter,komo)
-        ret = ry.NLP_Solver(komo.nlp(), verbose=0).solve()
-        q = komo.getPath()
     else:
-        #online_train(model, optimizer, criterion, new_graph_data, batch_size=1)
-        pass
+        #online_train(model, 0.001, new_graph_data, batch_size=1)
+        if checkpoint_counter == 0:
+            building_order = create_plan(target_folder,model,option=1)[0]
+        else:
+            building_order = create_plan(target_folder,model,graph=graph,option=2)[0]
+    komo = ry.KOMO(C, 3, 30, 1, False)
+    obj_index = building_order[0]
+    define_optimization(C,obj_index,komo)
+    ret = ry.NLP_Solver(komo.nlp(), verbose=0).solve()
+    q = komo.getPath()
 
--- planner_utils.py
+++ planner_utils.py
@@ -4,20 +4,24 @@ from torch_geometric.data import DataLoader
 from graph_utils import *
 import numpy as np
 import networkx as nx
+import copy
 
 
-def create_plan(target_folder,model):
-    target_graphs, positions = process_all_g_files(target_folder)
-    target_pyg = [convert_to_pyg_data(graph) for graph in target_graphs][0]
+def create_plan(target_folder,model,graph=None,option=1):
+    if option == 1:
+        target_graphs, positions = process_all_g_files(target_folder)
+        target_pyg = [convert_to_pyg_data(graph) for graph in target_graphs][0]
+    elif option == 2:
+        target_pyg = graph
     original_indices = list(range(target_pyg.num_nodes))
     removal_order = []
     model.eval()
+    target_pyg_copy = copy.deepcopy(target_pyg)
     with torch.no_grad():
         while target_pyg.num_nodes > 1:
             prediction = model(target_pyg)
             prediction = prediction.squeeze()
             predicted_probs = torch.sigmoid(prediction)
-
             max_prob_node = predicted_probs.argmax().item()
             removal_order.append(original_indices[max_prob_node])
             target_pyg = remove_node_and_edges(target_pyg, max_prob_node)
@@ -25,11 +29,11 @@ def create_plan(target_folder,model):
         if target_pyg.num_nodes == 1:
             remaining_node_original_index = original_indices[0]
             removal_order.append(remaining_node_original_index)
+            step_forward = remove_node_and_edges(target_pyg_copy, remaining_node_original_index)
 
-    return removal_order[::-1]
+    return removal_order[::-1],step_forward
 
 def remove_node_and_edges(data, node_idx):
-    # Create a mask for the nodes to keep
     node_mask = torch.ones(data.num_nodes, dtype=torch.bool)
     node_mask[node_idx] = False
     data.x = data.x[node_mask]
