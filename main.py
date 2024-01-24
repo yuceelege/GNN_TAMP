87898f0557f24623be19635bb4c5e38636325d6f
lira_bot <lira@cs.bilkent.edu.tr>
2024-01-24 13:40:03 +0300

    main update

--- main.py
+++ main.py
@@ -4,17 +4,18 @@ from gnn import *
 from komo_motion import *
 
 import os
+import sys
 import random
 import robotic as ry
 import time
-
+import contextlib
 
 def optimize(obj_index):
     komo = ry.KOMO(C, 3, 30, 1, False)
     define_optimization(C,obj_index,komo)
     ret = ry.NLP_Solver(komo.nlp(), verbose=0).solve()
     q = komo.getPath()
-    return q,ret
+    return q,ret,komo
 
 in_channels = 1  
 hidden_channels = 32 
@@ -28,11 +29,12 @@ model.load_state_dict(torch.load(model_path))
 target_graph, pos = process_all_g_files('target')
 correct_graph_edge_indices(target_graph)
 target_pyg = [convert_to_pyg_data(graph) for graph, positions in zip(target_graph, pos)][0]
-test_data = target_pyg
+#test_data = target_pyg
 target_folder = 'target'
 
 plan = create_plan(target_folder,model,option=1)
 building_order = plan[0]
+print(building_order)
 
 print("Building Order:", building_order)
 object_number = len(building_order)
@@ -50,27 +52,28 @@ plan_lst = []
 while objective_realized != True:
     if checkpoint_counter == 0:
         obj_index = building_order[checkpoint_counter]
-        q,ret = optimize(obj_index)
+        q,ret,komo = optimize(obj_index)
     if ret.feasible:
         if checkpoint_counter !=0:
-            C.setJointState(last_state)
+            C.setFrameState(last_state)
         checkpoint_counter += 1
         graph = plan[1]
         plan = create_plan(target_folder,model,graph=graph,option=2)
         building_order = plan[0]
+        print(building_order)
         obj_index = building_order[0]
-        q, ret = optimize(obj_index)
-        for t in range(q.shape[0]):
-            states.append(q[t])
-            if t == q.shape[0]-1:
-                last_state = q[t]
+        q, ret,komo = optimize(obj_index)
+        waypoints = komo.getPath_qAll()
+        states.append(komo.getFrameState(len(waypoints)-1))
+        last_state = komo.getFrameState(len(waypoints)-1)
+        #print(last_state)
         if checkpoint_counter == num_objects -1:
             init_komo(object_number,C2,pos)
-            komo2 = ry.KOMO(C, 3, 30, 1, False)
             for s in states:
-                C2.setJointState(s)
-                time.sleep(.1)
-            komo2.view_play(True, .1)
+                C2.setFrameState(s)
+                time.sleep(.5)
+                C2.view(True)
+            print("done")
             objective_realized =True
             break
     else:
